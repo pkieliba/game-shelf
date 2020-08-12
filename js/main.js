5f5d36d545570268b7586634cbf6b08de4e2ce03
pkieliba <pkieliba@gmail.com>
2020-08-12 10:36:06 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".js-range-slider").ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: 0,
+    max: 180,
+    step: 20,
+    from: 60,
+    to: 120,
+    grid: true,
+    decorate_both: false,
+    values_sepataror: " - ",
+    max_postfix: '+ ',
+});
