c65d994ffa1ccd6910dc39350ea89001914b8bb2
pkieliba <pkieliba@gmail.com>
2020-09-27 22:43:21 +0200

    Adds an example boardGames library, adds artwork for the games in the example library, imporves the readability of the main javascript file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Game Shelf
+
+Game shelf is a simple **randomisation tool**, that helps you choose which of your (many) board games you should play on a given day.
+
+----
+
+Game shelf allows you to **filter your board games' database** based on the number of players, desired game time, language, type (cooperative or competitive) and complexity. It then **randomly chooses** one of the games that meet the selected criteria.
+
+To help you choose the right game, the game shelf requires you to read in a **.csv file** containing your board games' database. The database **needs to contain the following collums** *(see the boardGames.csv file for an example)*:
+
+* GAME *(game name)*
+* MIN_PLAYERS *(minimum number of players required)*
+* MAX_PLAYERS *(maximum number of players allowed)*
+* MIN_TIME *(minimum time required)*
+* MAX_TIME *(maximum time required)*
+* COMPLEXITY *(game complexity on a scale of 1-5)*
+* LANGUAGE *(language of the game, use NA if no specific language required)*
+* TYPE *(game type, cooperative or competitive)*
+* TEAM *(team vs solo game, use 'yes' for team and 'no' for solo games)*
+
+
+
+Currently, this tool works only with the .csv files, containing all the required collumns and information. Similarly, it only provides artwork for the games included in the example *boardGames.csv* database. The only game languages supported at the moment are ENG (English), PL (Polish) and NA (no specific language required). 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var inputFile = document.getElementById('customFileInput');
+var inputFile = document.getElementById("customFileInput");
 var gameLibrary;
 var filteredLibrary;
 var finalLibrary;
 var chosenGame;
-var complexValues = ['very simple', 'simple', 'medium', 'complex', 'very complex'];
-var numPlayers = parseInt(document.querySelector('input[name="players"]:checked').value);
+var complexValues = ["very simple", "simple", "medium", "complex", "very complex"];
+var numPlayers = parseInt(document.querySelector("input[name='players']:checked").value);
 var maxTime = 120;
 var minTime = 60;
 var langs = {eng: true, pl: false};
@@ -16,7 +16,7 @@ var solo = {solo: true, team: false};
 /* INIT */
 
 $(document).ready(function () {
-    bsCustomFileInput.init()
+    bsCustomFileInput.init();
  });
 
 /* SLIDERS' BEHAVIOUR */
@@ -29,15 +29,16 @@ $(".time-slider").ionRangeSlider({
     step: 20,
     from: 60,
     to: 120,
-    grid: true, 
+    grid: true,
     decorate_both: false,
     values_sepataror: " - ",
-    max_postfix: '+ ',
+    max_postfix: "+ ",
     onFinish: function(e) {
-        minTime = $(".time-slider").data('ionRangeSlider').result.from;
-        maxTime = $(".time-slider").data('ionRangeSlider').result.to;
+        minTime = $(".time-slider").data("ionRangeSlider").result.from;
+        maxTime = $(".time-slider").data("ionRangeSlider").result.to;
         if (gameLibrary != undefined){
-            filterData(gameLibrary)}
+            filterData(gameLibrary);
+        }
     }
 });
 
@@ -49,10 +50,11 @@ $(".complex-slider").ionRangeSlider({
     decorate_both: false,
     values_sepataror: " - ",
     onFinish: function(e) {
-        minComplex = $(".complex-slider").data('ionRangeSlider').result.from+1;
-        maxComplex = $(".complex-slider").data('ionRangeSlider').result.to+1;
-        if (gameLibrary != undefined){ 
-            filterData(gameLibrary)}
+        minComplex = $(".complex-slider").data("ionRangeSlider").result.from+1;
+        maxComplex = $(".complex-slider").data("ionRangeSlider").result.to+1;
+        if (gameLibrary != undefined){
+            filterData(gameLibrary);
+        }
     }
 });
 
@@ -61,86 +63,103 @@ $(".complex-slider").ionRangeSlider({
 inputFile.onchange = function(e) {
   var ext = this.value.match(/\.([^\.]+)$/)[1];
   switch (ext) {
-    case 'csv':
-        ParseInput(this.files[0])
+    case "csv":
+        ParseInput(this.files[0]);
         break;
     default:
-      alert('File type not allowed. Please choose a .csv .xls or .xlsx file.');
-      this.value = '';
+      alert("File type not allowed. Please choose a .csv .xls or .xlsx file.");
+      this.value = "";
   }
 };
 
  /* GET NUMBER OF PLAYERS */
 
- $('.numPlay').click(function () { 
-    numPlayers = parseInt(document.querySelector('input[name="players"]:checked').value);
+ $(".numPlay").click(function () {
+    numPlayers = parseInt(document.querySelector("input[name='players']:checked").value);
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
+        filterData(gameLibrary);
+    }
+});
 
 /* GET LANGUAGES */
 
-$('#English').click(function () {
-    langs.eng = $('#English').is(':checked')
+$("#English").click(function () {
+    langs.eng = $("#English").is(":checked");
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
-$('#Polish').click(function () {
-    langs.pl = $('#Polish').is(':checked')
+        filterData(gameLibrary);
+    }
+});
+
+$("#Polish").click(function () {
+    langs.pl = $("#Polish").is(":checked");
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
+        filterData(gameLibrary);
+    }
+});
 
 /* GET GAME TYPE */
 
-$('#compete').click(function () {
-    gameType.compete = $('#compete').is(':checked')
+$("#compete").click(function () {
+    gameType.compete = $("#compete").is(":checked");
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
-$('#coop').click(function () {
-    gameType.coop = $('#coop').is(':checked')
+        filterData(gameLibrary);
+    }
+});
+
+$("#coop").click(function () {
+    gameType.coop = $("#coop").is(":checked");
     if (gameType.coop == true) {
-        solo.team = true
-        $('#team').prop('checked', true)}
+        solo.team = true;
+        $('#team').prop('checked', true);
+    }
     if (gameLibrary != undefined){
-    filterData(gameLibrary)}
-})
+    filterData(gameLibrary);
+    }
+});
 
 /* SOLO VS TEAM */
 
-$('#solo').click(function () {
-    solo.solo = $('#solo').is(':checked')
+$("#solo").click(function () {
+    solo.solo = $("#solo").is(":checked");
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
-$('#team').click(function () {
-    solo.team = $('#team').is(':checked')
+        filterData(gameLibrary);
+    }
+});
+
+$("#team").click(function () {
+    solo.team = $("#team").is(":checked");
     if (gameLibrary != undefined){
-        filterData(gameLibrary)}
-})
+        filterData(gameLibrary);
+    }
+});
 
 /* REROLL BUTTON */
 
-$('#reroll-button').click(function () {
-    chooseRandom(finalLibrary)
-})
+$("#reroll-button").click(function () {
+    chooseRandom(finalLibrary);
+});
 
 /* PARSE GAME LIBRARY FROM THE INPUT FILE */
 
 function ParseInput(file) {
-    Papa.parse(file, {header: true, complete: function(results, file){console.log('Input parsed correctly'); gameLibrary = results.data; filterData(gameLibrary)}})
+    Papa.parse(file, {header: true, complete: function(results, file){
+        console.log('Input parsed correctly'); gameLibrary = results.data; filterData(gameLibrary);
+    }});
 }
 
 function filterData(library) {
-    document.querySelector(".library").style.visibility = "visible"
+    document.querySelector(".library").style.visibility = "visible";
     if (gameType.coop == false && gameType.compete == false) {
-        $('span.filteredGames').text("Choose at least one game type (cooperative and/or competitive)")
-        document.querySelector(".filtered-pics").innerHTML = "" 
+        $('span.filteredGames').text("Choose at least one game type (cooperative and/or competitive)");
+        document.querySelector(".filtered-pics").innerHTML = "";
+        document.querySelector("#reroll-button").style.visibility= "hidden";
+        document.querySelector(".chosen").style.display= "none";
     }
     else if (solo.solo == false && solo.team == false) {
-        $('span.filteredGames').text("Choose solo and/or team games") 
-        document.querySelector(".filtered-pics").innerHTML = "" 
+        $("span.filteredGames").text("Choose solo and/or team games");
+        document.querySelector(".filtered-pics").innerHTML = "";
+        document.querySelector("#reroll-button").style.visibility = "hidden";
+        document.querySelector(".chosen").style.display= "none";
     }
     else {
         filteredLibrary = jQuery.grep(library, function(obj){ return (
@@ -151,7 +170,7 @@ function filterData(library) {
             the min time is treated inclusively, the max time exclusively */
             (minTime <= parseInt(obj.MAX_TIME) && parseInt(obj.MIN_TIME) < maxTime) &&
             /* language check */
-            ((langs.eng == true && langs.pl == true) ? (obj.LANGUAGE == 'ENG' || obj.LANGUAGE == "PL" || obj.LANGUAGE == "NA") : (
+            ((langs.eng == true && langs.pl == true) ? (obj.LANGUAGE == "ENG" || obj.LANGUAGE == "PL" || obj.LANGUAGE == "NA") : (
                 langs.eng == true ? (obj.LANGUAGE == "ENG" || obj.LANGUAGE == "NA") : (
                     langs.pl == true ? (obj.LANGUAGE == "PL" || obj.LANGUAGE == "NA") : obj.LANGUAGE == "NA"))) &&
             /* game type */
@@ -159,57 +178,74 @@ function filterData(library) {
                 gameType.compete == true ? (obj.TYPE == "competitive") : (
                     gameType.coop == true ? obj.TYPE == "cooperative" : (obj.TYPE != "competitive" && obj.TYPE != "cooperative" )))) &&
             /* complexity */
-            parseInt(obj.COMPLEXITY) >= minComplex && 
+            parseInt(obj.COMPLEXITY) >= minComplex &&
             parseInt(obj.COMPLEXITY) <= maxComplex &&
             /* solo vs team */
             ((solo.solo == true && solo.team == true) ? (obj.TEAM == "no" || obj.TEAM == "yes") : (
                 solo.solo == true ? (obj.TEAM == "no") : (
-                    solo.team == true ? obj.TEAM == "yes" : (obj.TEAM != "yes" && obj.TEAM != "no" )))) 
+                    solo.team == true ? obj.TEAM == "yes" : (obj.TEAM != "yes" && obj.TEAM != "no" ))))
         )}).map(function(obj){return obj.GAME})
 
-        $('span.filteredGames').text("")
-        document.querySelector(".filtered-pics").style.visibility = "visible"
-        finalLibrary = filteredLibrary
-        document.querySelector(".filtered-pics").innerHTML = "" 
-        filteredLibrary.forEach(printGame)
-        chooseRandom(finalLibrary)
-        filteredLibrary.forEach(addListener)
+        $("span.filteredGames").text("");
+        document.querySelector(".chosen").style.display= "flex";
+        document.querySelector(".filtered-pics").style.visibility = "visible";
+        finalLibrary = filteredLibrary;
+        document.querySelector(".filtered-pics").innerHTML = "";
+        filteredLibrary.forEach(printGame);
+        chooseRandom(finalLibrary);
+        filteredLibrary.forEach(addListener);
     }
 }
 
 function printGame(item){
-    var itemName = item.replace(/ /g, '');
-    document.querySelector(".filtered-pics").innerHTML += 
-    '<div class="col games-box"><div class="filtered-container" data-toggle="tooltip" data-container="body" data-placement="right" data-html="true"' +
-    'title="<p><b>Players: </b>' + gameLibrary.find(x => x.GAME == item).MIN_PLAYERS + '-' + gameLibrary.find(x => x.GAME == item).MAX_PLAYERS +
-    '</p><b>Time: </b>' + (parseInt(gameLibrary.find(x => x.GAME == item).MIN_TIME) +  parseInt(gameLibrary.find(x => x.GAME == item).MAX_TIME))/2 +
-    '</p><p><b>Complexity: </b>' + complexValues[gameLibrary.find(x => x.GAME == item).COMPLEXITY-1] +
-    '</p><p><b>Language: </b>' + gameLibrary.find(x => x.GAME == item).LANGUAGE +
-    '</p>"><input class="game-check" type="checkbox" id=' + 
-    itemName + ' checked /><label class="game-lab" for=' + itemName + '></label> ' + item + '</div><p class="game-name">' + item + '</p></div>'
-    $('[data-toggle="tooltip"]').tooltip({delay: {"show": 1000}})
+    var itemName = item.replace(/ /g, "");
+    document.querySelector(".filtered-pics").innerHTML +=
+    "<div class='col games-box'><div id=" + itemName + "_container class='filtered-container' data-toggle='tooltip' data-container='body' data-placement='right' data-html='true'" +
+    "title='<p><b>Players: </b>" + gameLibrary.find(x => x.GAME == item).MIN_PLAYERS + "-" + gameLibrary.find(x => x.GAME == item).MAX_PLAYERS +
+    "</p><b>Time: </b>" + (parseInt(gameLibrary.find(x => x.GAME == item).MIN_TIME) +  parseInt(gameLibrary.find(x => x.GAME == item).MAX_TIME))/2 +
+    "</p><p><b>Complexity: </b>" + complexValues[gameLibrary.find(x => x.GAME == item).COMPLEXITY-1] +
+    "</p><p><b>Language: </b>" + gameLibrary.find(x => x.GAME == item).LANGUAGE +
+    "</p>'><input class='game-check' type='checkbox' id=" +
+    itemName + " checked /><label class='game-lab' for=" + itemName + "></label><span>" + item + "</span></div><p class='game-name'>" + item + "</p></div>";
+    /* change background of the container to game box image */
+    var imageBox = new Image();
+    imageBox.onload = function() {
+        document.getElementById(itemName + "_container").style.background = "url(img/gameBoxes/" + itemName.replace(/[^\w\s]|_/g, "") + ".jpg) 50% 75%/ cover";
+        document.getElementById(itemName + "_container").getElementsByTagName("span")[0].innerHTML = "";
+    };
+    imageBox.src = "img/gameBoxes/" + itemName.replace(/[^\w\s]|_/g, "") + ".jpg";
+    $("[data-toggle='tooltip']").tooltip({delay: {"show": 1000}});
 }
 
 function addListener(item) {
     var itemName = item.replace(/ /g, '');
-    toAttach = document.getElementById(itemName)
+    toAttach = document.getElementById(itemName);
     toAttach.addEventListener("click", function(){
-        let gameIndex = finalLibrary.indexOf(item)
+        let gameIndex = finalLibrary.indexOf(item);
         if (gameIndex < 0){
-            finalLibrary.push(item)
+            finalLibrary.push(item);
         }
         else {
-            finalLibrary.splice(gameIndex, 1)
+            finalLibrary.splice(gameIndex, 1);
             if (chosenGame == item) {
-                chooseRandom(finalLibrary)
+                chooseRandom(finalLibrary);
             }
         }
     });
 }
 
 function chooseRandom(games){
-    chosenGame = games[Math.floor(Math.random() * games.length)]
-    document.querySelector("#chosen-game").innerHTML = '<div class="col games-box"><div class="chosen-container">' + chosenGame + '</div></div>'
-    document.querySelector("#reroll-button").style.visibility = "visible"
+    chosenGame = games[Math.floor(Math.random() * games.length)];
+    var chosenName = chosenGame.replace(/ /g, '');
+    document.querySelector("#chosen-game").innerHTML = "<div class='col games-box'><div class='chosen-container'>" + chosenGame + "</div><p class='chosen-name'>" + chosenGame + "</p></div>";
+    var chosenBox = new Image();
+    chosenBox.onload = function() {
+        document.querySelector(".chosen-container:first-child").style.background = "url(img/gameBoxes/" + chosenName.replace(/[^\w\s]|_/g, "") + ".jpg) 50% 75%/ cover";
+        document.querySelector(".chosen-container:first-child").innerHTML = "";
+    };
+    chosenBox.src = "img/gameBoxes/" + chosenName.replace(/[^\w\s]|_/g, "") + ".jpg";
+    document.querySelector("#reroll-button").style.visibility = "visible";
 }
+
+
 
